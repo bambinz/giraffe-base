d036f2a921d7b7c5d4e89f8049c0df80e57ffcb8
kjjgibson <kennethjjgibson@gmail.com>
2014-01-27 12:43:46 +1100

    jdbc driver require application.rb

--- config/application.rb
+++ config/application.rb
@@ -58,7 +58,12 @@ module Grammar
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
-    
+
     config.assets.initialize_on_precompile = false
+
+    if defined? JRUBY_VERSION
+      require 'jdbc/mysql'
+      Jdbc::MySQL.load_driver
+    end
   end
 end
