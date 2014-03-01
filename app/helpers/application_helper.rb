d6865c3b37415a603e64677549f398148f104107
Michael Baumberger <michael@moroku.com>
2014-03-01 11:49:38 +1100

    Rebrand to monopoly banking

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,11 @@
 module ApplicationHelper
   
   def title(page_title)
-    content_for :title, page_title.to_s
+    if ENV['RAILS_ENV'] == "development"
+      content_for :title, "Dev | #{page_title.to_s} | Monopoly banking"
+    else
+      content_for :title, "#{page_title.to_s} | Monopoly banking"
+    end
   end
   
 end
